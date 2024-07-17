170b6ee75fe8943f944e35fa6bd1137c895a9478
Alexandre Guénette <aguenette@pointlaz.com>
2024-07-17 14:50:45 -0400

    rename recording script variables

--- scripts/start_recording.sh
+++ scripts/start_recording.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-CAMERA_INDEX=$1
-CALIBRATION_DISTANCE=$2
-RECORDING_PATH=$3
+CAMERA_INDEX_ARG=$1
+CALIBRATION_DISTANCE_ARG=$2
+RECORDING_PATH_ARG=$3
+
+CALIBRATION_DATE=$(date +%Y-%m-%d)
 
 echo "Start recording in 10 seconds..."
 
 sleep 10
 
 screen -dmS record
-screen -XS record exec rosbag record -O ${RECORDING_PATH}/${TODAY}${CALIBRATION_DISTANCE}/cam${CAMERA_INDEX} /multi_camera/image_raw_${CAMERA_INDEX}
+screen -XS record exec rosbag record -O "${RECORDING_PATH_ARG}/${CALIBRATION_DATE}/${CALIBRATION_DISTANCE_ARG}/${CALIBRATION_DATE}_${CALIBRATION_DISTANCE_ARG}_camera_${CAMERA_INDEX_ARG}" "/multi_camera/image_raw_${CAMERA_INDEX_ARG}"
 
 echo "Recording started."
