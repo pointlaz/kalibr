965b26c132c7ccc8914044ce21e22cb5ef7b28fc
Alexandre Guénette <aguenette@pointlaz.com>
2024-07-23 09:26:05 -0400

    refactor the calibration script

--- scripts/calibrate_cameras.sh
+++ scripts/calibrate_cameras.sh
@@ -1,36 +1,42 @@
 #!/bin/env bash
 
-CAMERA_INDEXES=({0..6})
+CAMERA_BASE_TOPIC=/multi_camera/image_raw
+CAMERA_INDEXES=(
+	0
+	1
+	2
+	3
+	4
+	5
+	6
+)
 
 for CAMERA_INDEX in "${CAMERA_INDEXES[@]}"; do
-	BAG_NAME="cam${CAMERA_INDEX}"
-	BAG_FULL_PATH="${BAG_BASE_PATH}/${BAG_NAME}.bag"
+	BAG_NAME="${CALIBRATION_DATE}_${CALIBRATION_DISTANCE}_camera_${CAMERA_INDEX}"
+	BAG_PATH="${BAGS_BASE_PATH}/${BAG_NAME}"
 
-	echo "Calibrating camera ${CAMERA_INDEX} using rosbag ${BAG_FULL_PATH}"
+	echo "Calibrating camera ${CAMERA_INDEX} using rosbag ${BAG_PATH}.bag ..."
 
 	rosrun kalibr kalibr_calibrate_cameras \
 	--models "${CAMERA_MODEL}-${DISTORTION_MODEL}" \
-	--bag "${BAG_FULL_PATH}" \
+	--bag "${BAG_PATH}.bag" \
 	--topics "${CAMERA_BASE_TOPIC}_${CAMERA_INDEX}" \
 	--mi-tol "${MUTUAL_INFORMATION_TOLERANCE}" \
 	--target /target.yaml \
 	--dont-show-report
 
-	CAMCHAIN_FILE="${BAG_BASE_PATH}/${BAG_NAME}-camchain.yaml"
-	REPORT_CAM_FILE="${BAG_BASE_PATH}/${BAG_NAME}-report-cam.pdf"
-	RESULTS_CAM_FILE="${BAG_BASE_PATH}/${BAG_NAME}-results-cam.txt"
-
-	RESULTS_DIR="${BAG_BASE_PATH}/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}"
-	mkdir -p "${RESULTS_DIR}"
+	CAMCHAIN_FILE="${BAG_PATH}-camchain.yaml"
+	REPORT_CAM_FILE="${BAG_PATH}-report-cam.pdf"
+	RESULTS_CAM_FILE="${BAG_PATH}-results-cam.txt"
 
 	if [ -f "$REPORT_CAM_FILE" ]; then
-		mv "${CAMCHAIN_FILE}"    "${RESULTS_DIR}/cam${CAMERA_INDEX}-camchain.yaml"
-		mv "${REPORT_CAM_FILE}"  "${RESULTS_DIR}/cam${CAMERA_INDEX}-report-cam.pdf"
-		rm "${RESULTS_CAM_FILE}"
+		RESULTS_DIR="${CALIBRATION_BASE_PATH}/${CALIBRATION_DATE}/${CALIBRATION_DISTANCE}/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}"
+		mkdir -p "${RESULTS_DIR}"
+		mv "${CAMCHAIN_FILE}"    "${RESULTS_DIR}/"
+		mv "${REPORT_CAM_FILE}"  "${RESULTS_DIR}/"
 	else
-		rm "${CAMCHAIN_FILE}"
-		rm "${RESULTS_CAM_FILE}"
+		rm "${CAMCHAIN_FILE}" 2>/dev/null
 	fi
-done
 
-echo "All camera calibrations are done."
+	rm "${RESULTS_CAM_FILE}" 2>/dev/null
+done
