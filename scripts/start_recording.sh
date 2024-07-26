d21c0e9202b9acae18af16519f5157f854b9356a
Alexandre Guénette <aguenette@pointlaz.com>
2024-07-26 14:21:02 -0400

    refactor start recording script

--- scripts/start_recording.sh
+++ scripts/start_recording.sh
@@ -2,27 +2,95 @@
 
 set -e
 
-CALIBRATION_DATE=$(date +%Y-%m-%d)
-
-RECORDING_BASE_PATH_ARG=$1
 DEFAULT_RECORDING_BASE_PATH="/hdd/Documents/LAZ_Data/Calibrations/Cameras"
+DEFAULT_BAG_DURATION=180
 
-if [[ -z "${RECORDING_BASE_PATH_ARG}" ]]; then
-	echo "No recording base path has been provided, the default recording base path will be used instead. The default value is: ${DEFAULT_RECORDING_BASE_PATH}"
+if [[ $# == 0 ]]; then
+    RECORDING_BASE_PATH_ARG="${DEFAULT_RECORDING_BASE_PATH}"
+    BAG_DURATION_ARG=$DEFAULT_BAG_DURATION
+elif [[ $# == 2 ]]; then
+    if [[ "$1" == "-p" || "$1" == "--path" ]]; then
+        BAG_DURATION_ARG=$DEFAULT_BAG_DURATION
+        if [[ -d "$2" ]]; then
+            RECORDING_BASE_PATH_ARG="$2"
+        else
+            echo "The recording path must be an existing directory."
+            RECORDING_BASE_PATH_ARG="${DEFAULT_RECORDING_BASE_PATH}"
+        fi
+    elif [[ "$1" == "-d" || "$1" == "--duration" ]]; then
+        RECORDING_BASE_PATH_ARG="${DEFAULT_RECORDING_BASE_PATH}"
+        if [[ $2 =~ ^[0-9]+$ ]]; then
+            BAG_DURATION_ARG=$2
+        else
+            echo "The duration must be a number in seconds."
+            BAG_DURATION_ARG=$DEFAULT_BAG_DURATION
+        fi
+    else
+        if [[ ("$1" =~ "-p" && "$1" != "-p") || ("$1" =~ "--p" && "$1" != "--path") ]]; then
+            echo "Wrong flag for the path."
+        elif [[ ("$1" =~ "-d" && "$1" != "-d") || ("$1" =~ "--d" && "$1" != "--duration") ]]; then
+            echo "Wrong flag for the duration."
+        fi
+        echo "Usage: start_recording.sh [(-p | --path) PATH] [(-d | --duration) DURATION]
+-p, --path PATH          The path to the recording directory.
+-d, --duration DURATION  The duration of the recording  in seconds."
+        exit 1
+    fi
+elif [[ $# == 4 ]]; then
+    if [[ ("$1" == "-p" || "$1" == "--path") && ("$3" == "-d" || "$3" == "--duration") ]]; then
+        if [[ -d "$2" ]]; then
+            RECORDING_BASE_PATH_ARG="$2"
+        else
+            echo "The recording path must be an existing directory."
+            RECORDING_BASE_PATH_ARG="${DEFAULT_RECORDING_BASE_PATH}"
+        fi
+        if [[ $4 =~ ^[0-9]+$ ]]; then
+            BAG_DURATION_ARG=$4
+        else
+            echo "The duration must be a number in seconds."
+            BAG_DURATION_ARG=$DEFAULT_BAG_DURATION
+        fi
+    elif [[ ("$1" == "-d" || "$1" == "--duration") && ("$3" == "-p" || "$3" == "--path") ]]; then
+        if [[ $2 =~ ^[0-9]+$ ]]; then
+            BAG_DURATION_ARG=$2
+        else
+            echo "The duration must be a number in seconds."
+            BAG_DURATION_ARG=$DEFAULT_BAG_DURATION
+        fi
+        if [[ -d "$4" ]]; then
+            RECORDING_BASE_PATH_ARG="$4"
+        else
+            echo "The recording path must be an existing directory."
+            RECORDING_BASE_PATH_ARG="${DEFAULT_RECORDING_BASE_PATH}"
+        fi
+    else
+        if [[ (("$1" != "-p" && "$1" != "--path") && ("$3" == "-d" || "$3" == "--duration")) ||
+            (("$1" == "-d" || "$1" == "--duration") && ("$3" != "-p" && "$3" != "--path")) ]]; then
+            echo "Wrong flag for the path."
+        elif [[ (("$1" == "-p" || "$1" == "--path") && ("$3" != "-d" && "$3" != "--duration")) ||
+            (("$1" != "-d" && "$1" != "--duration") && ("$3" == "-p" || "$3" == "--path")) ]]; then
+            echo "Wrong flag for the duration."
+        else
+            echo "Wrong flags for the path and the duration."
+        fi
+        echo "Usage: start_recording.sh [(-p | --path) PATH] [(-d | --duration) DURATION]
+-p, --path PATH          The path to the recording directory.
+-d, --duration DURATION  The duration of the recording  in seconds."
+        exit 1
+    fi
+else
+    echo "Wrong number of arguments. $# arguments were provided instead of 4.
+Usage: start_recording.sh [(-p | --path) PATH] [(-d | --duration) DURATION]
+-p, --path PATH          The path to the recording directory.
+-d, --duration DURATION  The duration of the recording in seconds."
+    exit 1
 fi
 
-RECORDING_PATH="${RECORDING_BASE_PATH_ARG:-$DEFAULT_RECORDING_BASE_PATH}/${CALIBRATION_DATE}/bags"
+CALIBRATION_DATE=$(date +%Y-%m-%d)
+RECORDING_PATH="${RECORDING_BASE_PATH_ARG}/${CALIBRATION_DATE}/Bags"
+BAG_DURATION="${BAG_DURATION_ARG}"
 
 mkdir -p "${RECORDING_PATH}"
-
-BAG_DURATION_ARG=$2
-BAG_DURATION_DEFAULT=180
-
-if [[ -z "${RECORDING_BASE_PATH_ARG}" ]]; then
-	echo "No bag duration has been provided, the default bag duration will be used instead. Default value is: ${BAG_DURATION_DEFAULT} seconds."
-fi
-
-BAG_DURATION="${BAG_DURATION_ARG:-$BAG_DURATION_DEFAULT}"
 
 CAMERA_INDEXES=(
     0
@@ -38,7 +106,7 @@ screen -dmS record
 
 for CAMERA_INDEX in "${CAMERA_INDEXES[@]}"; do
 
-    BAG_NAME="${CALIBRATION_DATE}_camera_${CAMERA_INDEX}"
+    BAG_NAME="camera_${CAMERA_INDEX}"
     BAG_PATH="${RECORDING_PATH}/${BAG_NAME}"
 
     echo "Camera $((CAMERA_INDEX + 1)) recording will start in 10 seconds..."
@@ -53,16 +121,15 @@ for CAMERA_INDEX in "${CAMERA_INDEXES[@]}"; do
 
     echo "Camera $((CAMERA_INDEX + 1)) recording is finished."
 
-    echo "The ${BAG_NAME} bag has been saved in the ${RECORDING_PATH} directory."
+    echo "The file '${BAG_NAME}.bag' has been saved in the '${RECORDING_PATH}' directory."
 
     if [[ "${CAMERA_INDEX}" -lt "${CAMERA_INDEXES[-1]}" ]]; then
-        echo "Prepare camera $((CAMERA_INDEX + 2)) now."
+        echo "Prepare the camera $((CAMERA_INDEX + 2)) now."
+        sleep 10
     fi
-
-    sleep 10
 
 done
 
-echo "Recording of all cameras is complete."
+echo "All camera recordings are completed."
 
 screen -XS record quit 1>/dev/null
