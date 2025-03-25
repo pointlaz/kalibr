7475da5ef9d0f411742aa131c44f41cfa9fe9d44
Alexandre Guénette <aguenette@pointlaz.com>
2025-03-25 16:50:59 -0400

    cleaning

--- scripts/camera_validator.sh
+++ scripts/camera_validator.sh
@@ -2,8 +2,6 @@
 
 CAMERA_INDEX=0
 
-# /home/pointlaz/Drive/lazaruss-01/Calibrations/Cameras/2024-07-16/1m
-
 echo "${BAGS_BASE_PATH}/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}/camera_${CAMERA_INDEX}-camchain.yaml"
 
 rosrun kalibr kalibr_camera_validator \
