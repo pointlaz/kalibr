6d03bd803cc1c6118a213311721dcaad50ec3c66
Alexandre Guénette <aguenette@pointlaz.com>
2025-07-31 08:56:09 -0400

    refactoring
    - Delete Docker Compose file and scripts for cameras acquisition and
    recording
    - Rename scripts for calibrations and validations
    - Refactor Makefile
    - Refactor template env file

--- scripts/camera_validator.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CAMERA_INDEX=0
-
-echo "${BAGS_BASE_PATH}/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}/camera_${CAMERA_INDEX}-camchain.yaml"
-
-rosrun kalibr kalibr_camera_validator \
---cam "${BAGS_BASE_PATH}/../${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}/camera_${CAMERA_INDEX}-camchain.yaml" \
---target /target.yaml \
--- scripts/start_calibrations.bash
+++ scripts/start_calibrations.bash
@@ -1,12 +1,14 @@
 #!/bin/env bash
 
-if [[ -z "${BAGS_BASE_PATH}" ]]; then
-	echo "BAGS_BASE_PATH variable must be set, i.e. A valid path to a directory containing the bags used for calibration."
+if [[ -z "${SCANNER_SN}" ]]; then
+	echo "'SCANNER_SN' variable must be set, i.e. A valid path to a directory containing the bags used for calibration."
 	exit 1
 fi
 
-if [ ! -d "${BAGS_BASE_PATH}" ]; then
-	echo "Directory '${BAGS_BASE_PATH}' does not exists. Aborting calibration..."
+RAW_DIR_PATH="${SCANNER_SN}/cameras/raw"
+
+if [ ! -d "${RAW_DIR_PATH}" ]; then
+	echo "Directory '${RAW_DIR_PATH}' does not exists. Create it before proceeding. Aborting calibration..."
 	exit 1
 fi
 
@@ -28,42 +30,34 @@ if [[ -z "${MUTUAL_INFORMATION_TOLERANCE}" ]]; then
 	MUTUAL_INFORMATION_TOLERANCE="${DEFAUL_MUTUAL_INFORMATION_TOLERANCE}"
 fi
 
-CAMERA_INDEXES=(
-	0
-	1
-	2
-	3
-	4
-	5
-	6
-)
+BAG_PATHS=( $( ls "${RAW_DIR_PATH}"/*.bag 2>/dev/null ) )
 
-for CAMERA_INDEX in "${CAMERA_INDEXES[@]}"; do
+if (( "${#BAG_PATHS[@]}" == 0 )); then
+	echo "Directory '${RAW_DIR_PATH}' is empty. Aborting calibration..."
+	exit 1
+fi
 
-	BAG_NAME="camera_${CAMERA_INDEX}"
-	BAG_PATH="${BAGS_BASE_PATH}/${BAG_NAME}"
+for BAG_PATH in "${BAG_PATHS[@]}"; do
 
-	if [[ ! -f "${BAG_PATH}.bag" ]]; then
-		echo "File '${BAG_PATH}.bag' does not exists. Aborting calibration..."
-		exit 1
-	fi
+	CAMERA_INDEX="${BAG_PATH#*camera-}"
+	CAMERA_INDEX="${CAMERA_INDEX%-calibration*}"
 
-	echo "Calibrating camera '${CAMERA_INDEX}' using the bag '${BAG_PATH}.bag' ..."
+	echo "Calibrating camera '${CAMERA_INDEX}' using the bag: '${BAG_PATH}' ..."
 
 	rosrun kalibr kalibr_calibrate_cameras \
 	--models "${CAMERA_MODEL}-${DISTORTION_MODEL}" \
-	--bag "${BAG_PATH}.bag" \
+	--bag "${BAG_PATH}" \
 	--topics "/multi_camera/image_raw_${CAMERA_INDEX}" \
 	--mi-tol "${MUTUAL_INFORMATION_TOLERANCE}" \
 	--target /target.yaml \
 	--dont-show-report
 
-	CAMCHAIN_FILE="${BAG_PATH}-camchain.yaml"
-	REPORT_CAM_FILE="${BAG_PATH}-report-cam.pdf"
-	RESULTS_CAM_FILE="${BAG_PATH}-results-cam.txt"
+	CAMCHAIN_FILE="${BAG_PATH%.bag}-camchain.yaml"
+	REPORT_CAM_FILE="${BAG_PATH%.bag}-report-cam.pdf"
+	RESULTS_CAM_FILE="${BAG_PATH%.bag}-results-cam.txt"
 
 	if [ -f "$REPORT_CAM_FILE" ]; then
-		RESULTS_PATH="${BAGS_BASE_PATH}/../results/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}"
+		RESULTS_PATH="${RAW_DIR_PATH}/../results/${CAMERA_MODEL}-${DISTORTION_MODEL}_mi-tol_${MUTUAL_INFORMATION_TOLERANCE}"
 		mkdir -p "${RESULTS_PATH}"
 		mv "${CAMCHAIN_FILE}"    "${RESULTS_PATH}/"
 		mv "${REPORT_CAM_FILE}"  "${RESULTS_PATH}/"
